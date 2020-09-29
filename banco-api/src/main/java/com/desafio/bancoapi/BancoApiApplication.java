5c6083a97cb328e08ffe0c010afc343354ac8a4b
Antonio Amorim <antonioreisamorim@gmail.com>
2020-09-29 16:54:22 -0300

    first commit

---
+++ banco-api/src/main/java/com/desafio/bancoapi/BancoApiApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.bancoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancoApiApplication.class, args);
+    }
+
+}
